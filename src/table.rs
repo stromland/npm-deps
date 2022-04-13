4d2817885359828090f41863ed64078c478c9305
Espen Strømland <espen.stromland@gmail.com>
2022-04-13 20:15:19 +0200

    fix: change crate for table printing

--- src/table.rs
+++ src/table.rs
@@ -1,25 +1,26 @@
-use prettytable::{Table, row, cell};
+use comfy_table::{Table};
 use crate::Dependency;
 
 pub fn print_dependencies(deps: Vec<Dependency>) {
     let mut table = Table::new();
-    table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+
+    table.load_preset(comfy_table::presets::UTF8_BORDERS_ONLY);
 
     // Header
-    table.set_titles(row!["Name", "Current version", "Latest version"]);
+    table.set_header(vec!["Name", "Current version", "Latest version"]);
 
     // Dependencies
     for dep in deps {
         if let Some(latest) = dep.latest_version {
             if dep.version != latest {
-                table.add_row(row![dep.name, c -> dep.version, bc -> latest]);
+                table.add_row(vec![dep.name, dep.version, latest]);
             }
         }
     }
 
-    if table.len() == 0 {
+    if table.row_iter().len() == 0 {
         println!("All dependencies are up to date!")
     } else {
-        table.printstd();
+        println!("{}", table);
     }
 }
